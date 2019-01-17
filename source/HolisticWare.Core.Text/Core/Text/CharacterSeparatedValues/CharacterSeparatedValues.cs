aa97bbfab43b40ad862dce745a5ba67cc1044d13
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-01-17 15:27:45 +0100

    extraction

--- source/HolisticWare.Core.Text/Core/Text/CharacterSeparatedValues/CharacterSeparatedValues.cs
+++ source/HolisticWare.Core.Text/Core/Text/CharacterSeparatedValues/CharacterSeparatedValues.cs
@@ -1,33 +1,6 @@
-﻿// /*
-//    Copyright (c) 2018-4
-//
-//    moljac
-//    CharacterSeparatedValues.cs
-//
-//    Permission is hereby granted, free of charge, to any person
-//    obtaining a copy of this software and associated documentation
-//    files (the "Software"), to deal in the Software without
-//    restriction, including without limitation the rights to use,
-//    copy, modify, merge, publish, distribute, sublicense, and/or sell
-//    copies of the Software, and to permit persons to whom the
-//    Software is furnished to do so, subject to the following
-//    conditions:
-//
-//    The above copyright notice and this permission notice shall be
-//    included in all copies or substantial portions of the Software.
-//
-//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-//    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-//    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-//    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-//    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-//    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-//    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-//    OTHER DEALINGS IN THE SOFTWARE.
-// */
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.Threading.Tasks;
 
 namespace Core.Text
 {
@@ -35,57 +8,45 @@ namespace Core.Text
     {
         public CharacterSeparatedValues()
         {
-            Separators = new string[] { ",", ";", " ", @"\t", };
-            SeparatorsNewLine = new string[] { Environment.NewLine };
-            CommentStrings = new string[] { "#", "//" };
-            HasHeader = false;
-            NumberFormatInfo = CultureInfo.CurrentCulture.NumberFormat;
+            ContainedType = typeof((string Ivek, string Jozo));
 
             return;
         }
 
-        public string[] Separators 
-        { 
-            get; 
-            set; 
-        }
-
-        public string[] SeparatorsNewLine
+        public string Text
         {
             get;
             set;
         }
 
-        public string[] CommentStrings 
-        { 
-            get; 
-            set; 
+        public async Task<string> LoadAsync(string filename)
+        {
+            using
+                (
+                    System.IO.FileStream stream = System.IO.File.Open
+                                                                    (
+                                                                        filename,
+                                                                        System.IO.FileMode.Open,
+                                                                        System.IO.FileAccess.Read
+                                                                    )
+                )
+            using
+                (
+                    System.IO.TextReader tr = new System.IO.StreamReader(stream)
+                )
+            {
+                Text = await tr.ReadToEndAsync();
+            }
+
+            return Text;
         }
 
-        public bool HasHeader 
-        { 
-            get; 
-            set; 
-        }
-
-        public NumberFormatInfo NumberFormatInfo
+        public IEnumerable<string> ColumnNames
         {
             get;
             set;
         }
 
-        public Func
-                <
-                    string, 
-                    IEnumerable<string>, 
-                    NumberFormatInfo, 
-                    string[], 
-                    IEnumerable<string[]>
-                > ParseMethod
-        {
-            get;
-            set;
-        }
 
     }
 }
