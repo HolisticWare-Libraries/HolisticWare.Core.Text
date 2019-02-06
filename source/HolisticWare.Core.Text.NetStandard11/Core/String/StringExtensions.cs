ee78bd04d41eb94eb647e372b07707b34099fa35
someone1984 <sasabajtl@gmail.com>
2019-02-06 11:16:08 +0100

    Split string without empty spaces (span/memory)

--- source/HolisticWare.Core.Text.NetStandard11/Core/String/StringExtensions.cs
+++ source/HolisticWare.Core.Text.NetStandard11/Core/String/StringExtensions.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace Core.Text.Core.String
+namespace Core.Strings
 {
     public static class StringExtensions
     {
-        public static IEnumerable<string> SplitWithMemory(this string s, char character)
+        public static IEnumerable<string> SplitWithCharacter(this string s, char character)
         {
             ReadOnlyMemory<char> memory = s.AsMemory();
 
@@ -40,5 +39,51 @@ namespace Core.Text.Core.String
                 yield return memory.Span.Slice(last_item + 1).ToString();
             }
         }
+
+        public static IEnumerable<string> SplitWithoutEmptySpaces(this string s, string[] separators)
+        {
+            ReadOnlyMemory<char> readOnlyMemory = s.AsMemory();
+            int length = readOnlyMemory.Span.Length;
+            ReadOnlyMemory<char> separator_item = new ReadOnlyMemory<char>();
+            int start_index = 0;
+
+            for (int i = start_index; i < length; i++)
+            {
+                for (int j = 0; j < separators.Length; j++)
+                {
+                    separator_item = separators[j].AsMemory();
+                    if (i + separator_item.Length < length)
+                    {
+                        if (readOnlyMemory.Span.Slice(i, separator_item.Length).SequenceEqual(separator_item.Span))
+                        {
+                            int length_of_item = i - start_index;
+                            ReadOnlySpan<char> splitted = readOnlyMemory.Span.Slice(start_index, length_of_item);
+                            if (!splitted.IsEmpty)
+                            {
+                                yield return splitted.ToString();
+                            }
+                            start_index = i + separator_item.Length;
+                        }
+                    }
+                }
+
+                if (i + separator_item.Length == length)
+                {
+                    int length_of_item = i - start_index;
+                    if (length_of_item < 0)
+                    {
+                        length_of_item = length - start_index;
+                    }
+                    ReadOnlySpan<char> splitted = readOnlyMemory.Span.Slice(start_index, length_of_item);
+                    if (!splitted.IsEmpty)
+                    {
+                        yield return splitted.ToString();
+                    }
+
+                    start_index = i + length_of_item;
+                }
+            }
+        }
+
     }
 }
