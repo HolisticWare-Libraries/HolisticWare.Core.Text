a81db0e6fd4d6f3827fd8224faedcfdd03a57342
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-04-10 14:50:33 +0200

    old source moved into new namespace new unit tests

---
+++ source/HolisticWare.Core.Text/Core/Text/CharacterSeparatedValues/old/CharacterSeparatedValues.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Core.Text
+{
+    public partial class CharacterSeparatedValues
+    {
+        public CharacterSeparatedValues()
+        {
+            Separators = new string[] { ",", ";", " ", @"\t", };
+            SeparatorsNewLine = new string[] { Environment.NewLine, @"\n" };
+            CommentStrings = new string[] { "#", "//" };
+            HasHeader = false;
+            NumberFormatInfo = CultureInfo.CurrentCulture.NumberFormat;
+
+            ContainedType = typeof((string Ivek, string Jozo));
+
+            return;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<string> ColumnNames
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
